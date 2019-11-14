15dd2cad6ead242ae1483ed6d44ec38bdde4745e
Willy Huang <willy303156@gmail.com>
2019-11-15 03:10:41 +0800

    "complete ver1, no optimize"

--- container.h
+++ container.h
@@ -3,8 +3,6 @@
 
 #include <time.h>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <fstream>
 
 using namespace std;
@@ -18,7 +16,6 @@ using namespace std;
 class node
 {
     friend class graph;
-    friend class BST;
 
     private:
     int row, col;
@@ -26,7 +23,6 @@ class node
     int state;
     bool travel;
     node *parent;
-//    node *child[4];
     node *direction[4];
     node *Left, *Right;
     node *order;
@@ -34,41 +30,16 @@ class node
     public:
     node(){}
     node(int row, int col, int state):row(row), col(col), height(1), state(state), travel(false), 
-                                        parent(nullptr), Left(nullptr), Right(nullptr)/*, 
-                                        up(nullptr), down(nullptr)*/
+                                        parent(nullptr), Left(nullptr), Right(nullptr)
                                         {
                                             for (int i = 0; i < 4; i++)
                                             {    
                                                 direction[i] = nullptr;
-                            //                    child[i] = nullptr;
                                             }
                                         }
-/*    void set_state(char state){this->state = state;}
-    void set_travel(bool travel){this->travel = travel;}
-    void set_parent(node *parent){this->parent=parent;}
-    void set_direction(int index, node* direction){this->direction[index] = direction;}
-    void set_child(int index, node* child){this->child[index] = child;}
-    void set_height(int height){this->height = height;}
-    int get_row(){return this->row;}
-    int get_col(){return this->col;}
-    int get_state(){return this->state;}
-    int get_height(){return this->height;}
-    bool get_travel(){return this->travel;}
-    node *get_parent(){return this->parent;}
-    node *get_direction(int index){return this->direction[index];}
-    node *get_child(int index){return this->child[index];}*/
-    void print_data(ofstream& file){file << this->row << ' ' << this->col  << '\n';}    
-    void print_data(int battery, ofstream& file){file << this->row << ' ' << this->col << ' ' << battery<<  '\n';} 
-    void print_data(){cout << this->row << ' ' << this->col << ' ' << this->height << '\n';}
-    void print_data(int battery){cout << this->row << ' ' << this->col << ' ' << this->height <<' '<< battery << '\n';}
+
+    void print_data(ofstream& file){file << this->row << ' ' << this->col  << '\n';}
     
-    int get_state(){return this->state;}
-    bool check_battery(int battery)
-    {
-        if (this->height - 1 <= battery)
-            return true;
-        else return false;
-    }
     bool check_battery_short(int step, int height, int const battery_max)
     {
         if ((step + height) <= (battery_max / 2))
@@ -76,76 +47,72 @@ class node
         else return false;
     }
 
-    node *get_order(){return this->order;}
-    
-    int get_height(){return this->height;}
 };
-
-
-
-class BST
-{
-    private:
-    node *root;
-    node* insert(node *root, node* target)
-    {
-    if (root == nullptr)
-    {
-      //      cout << "here\n";
-        return target;
-    }
-    else if (root->height == target->height)
-    { 
-        root->Left = insert(root->Left, target);
-    }
-    else if (root->height > target->height)
-    { 
-        root->Left = insert(root->Left, target);
-    }
-    else if (root->height < target->height)
-    {
-        root->Right = insert(root->Right, target);
-    }
-    return root;
-    }
-    public:
-
-    BST(node *root):root(root){}
-    void insert(node* target)
-    {
-        if (root == target) return;
-        root = insert(root, target);
-    }
-};
-
 
 class graph
 {
     private:
     int row_max, col_max;
     int row_begin, col_begin;
-    bool all_visit(node *root);
-    node *sort(node *root, BST &BST);
-    void sort(node *root, node **arr, int &counter);
     node *root;
     public:
     
-
     int element_number;
     node ***map;
     graph(int row, int col, ifstream& file);
     ~graph();
     void link(int row, int col);
-    void traverse(node *root, int &battery, const int battery_max, int &counter, ofstream& file_out);
     void print(int row, int col);
     void reset_travel();
-    bool all_visit();
     void preorder(node *root);
     node *MST();
     void shortest_path(node *root, const int battery_max, int &counter, node *highest,ofstream& file_out);
-    void sort(node **arr);
     node* BFS();
 };
 
+class stack
+{
+    private:
+        int index;
+        int capacity;
+        node **arr;
+        void new_arr();
+    public:
+        stack():index(-1),capacity(1){
+            arr = new node*[capacity];
+        }
+        void push(node *s);
+        void pop();
+        node *top();
+        bool empty();
+};
+
+class node_queue
+{
+    friend class queue;
+
+    private:
+        node * data;
+        node_queue *next;
+
+        node_queue():data(nullptr),next(nullptr){};
+        node_queue(node *data):data(data),next(nullptr){};
+};
+
+class queue
+{
+    private:
+        node_queue *front_node;
+        node_queue *back_node;
+        int size;
+
+    public:
+        void push(node *q);
+        void pop();
+        bool empty();
+        node *front();
+
+        queue():front_node(nullptr),back_node(nullptr),size(0){};
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
 #include "container.h"
 
-
-
-
 int main()
 {
     int row, col, Battery;
     ifstream file_in;
     ofstream file_out1;
-    ifstream file_out2;
-    ofstream file_out;
     clock_t start_t, end_t;
 
     file_in.open("floor.data", ios::in);
-    file_out1.open("temp.path", ios::out | ios::trunc);
+    file_out1.open("final.path", ios::out | ios::trunc);
     if (!file_in.is_open())
     {
         cout << "open file fail!\n";
@@ -29,15 +24,7 @@ int main()
     start_t = clock();
     graph g(row, col, file_in);
 
-    cout << endl << endl << endl;
-
-/*   for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-            cout << g.map[i][j]->get_state();
-        cout << endl;
-    }*/
-
+    file_out1 << "                 " << '\n';
 
     node *begin;
 
@@ -52,28 +39,15 @@ int main()
     
     order = g.BFS();        // biggest height
  
-/*    while (order !=begin)
-    {    
-        order->print_data();
-        order = order->get_order();
-    }*/
-
-    
-//    node *arr[g.element_number];
-/*
-cout << "here\n";
-    g.sort(arr);*/
-
- /*   for (int i = 0; i < 8; i++)
-        cout << arr[i]->get_height() <<endl;
-*/
-
-  //  g.traverse(begin, battery, Battery, count, file_out);
     g.shortest_path(begin, Battery, count, order, file_out1);
-    cout << count << endl;
-    cout << endl;
 
+    cout << count << endl;
+
+    file_out1.seekp(0L, ios::beg);
+    file_out1 << count;
     file_in.close();
+
+
     if (file_in.is_open())
     {
         cout << "Close file fails!\n";
@@ -81,24 +55,6 @@ cout << "here\n";
     }
     file_out1.close();
     if (file_out1.is_open())
-    {
-        cout << "Close file fails!\n";
-        return 0;
-    }
-    file_out2.open("temp.path",ios::in);
-    file_out.open("final.path",  ios::out | ios::trunc);
-    file_out << count << '\n';
- //   while (file_)
-
-    file_out2.close();
-    file_out.close();
-
-    if (file_out2.is_open())
-    {
-        cout << "Close file fails!\n";
-        return 0;
-    }
-    if (file_out.is_open())
     {
         cout << "Close file fails!\n";
         return 0;
